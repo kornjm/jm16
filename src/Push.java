5f0d3a5409403ec6de8a63b98f47db32cc64445a
kornjm <65711697+kornjm@users.noreply.github.com>
2021-10-07 07:49:36 +0900

    Update Push.java

--- src/Push.java
+++ src/Push.java
@@ -1,5 +1,5 @@
 public class Push {
     public static void main(String[] args){
-        System.out.println("Push~~java before jungmo  modify");
+        System.out.println("Push~~java before jungmo  modify 수정후");
     }
 }
